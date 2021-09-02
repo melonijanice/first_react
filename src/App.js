62c730c87f9d126f7f72f41651b0761a9198625e
Janice Mathias <melonijanice@gmail.com>
2021-09-01 22:10:49 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import Welcome from './components/FirstPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div><h2>Hello Dojo</h2>
+   <Welcome></Welcome>
+   </div>
   );
 }
 
